3d20938690f44c190c0eb1d1e9c8d313fb173a9b
Justin Michalicek <jmichalicek@gmail.com>
2017-05-06 01:12:28 -0400

    Rename to WorryWort
    * change all Brewbase to WorryWort in text and filenames
    * change all BrewBase to WorryWort in text and filenames
    * change all brewbase to worrywort in text and filenames

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class BrewbaseClient extends Component {
+export default class WorryWortClient extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('BrewbaseClient', () => BrewbaseClient);
+AppRegistry.registerComponent('WorryWortClient', () => WorryWortClient);
